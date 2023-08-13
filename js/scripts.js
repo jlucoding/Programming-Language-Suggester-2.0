afa789b66c6f41cd81ef669198c088d14f8ae933
Jonathan Lu <jdlu.coding@gmail.com>
2023-08-13 16:02:48 -0700

    WIP: Separate UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
-$(document).ready(function () {
-  
+    const reason = document.querySelector("input:radio[name='reason']:checked").val;
+    const response = document.querySelector("input:radio[name='response']:checked").val;
+    const way = document.querySelector("input:radio[name='way']:checked").val;
+    const car = document.querySelector("input:radio[name='car']:checked").val;
+    const platform = document.querySelector("input:radio[name='platform']:checked").val;
+    const os = document.querySelector("input:radio[name='os']:checked").val;
+    const tendency = document.querySelector("input:radio[name='tendency']:checked").val;
+    const company = document.querySelector("input:radio[name='company']:checked").val;
 
-  $("form#survey").submit(function (event) {
+window.addEventListener('load', function () {
+
+  let form = document.querySelector("form");
+  form.addEventListener('submit',  function (event) {
     event.preventDefault();
 
-    const reason = $("input:radio[name=reason]:checked").val();
-    const response = $("input:radio[name=response]:checked").val();
-    const way = $("input:radio[name=way]:checked").val();
-    const car = $("input:radio[name=car]:checked").val();
-    const platform = $("input:radio[name=platform]:checked").val();
-    const os = $("input:radio[name=os]:checked").val();
-    const tendency = $("input:radio[name=tendency]:checked").val();
-    const company = $("input:radio[name=company]:checked").val();
+    
   });
 });
